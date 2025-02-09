59b9c2a4c5b8fd6bbc7f257c86264eba4aabbaba
rajeshprivate007 <rajesh.sahu.private@gmail.com>
2025-02-09 18:11:22 +0530

    slijls

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now. This is version2 
+          Learn React Now. This is version2.
         </a>
       </header>
     </div>
